6053c6ee825cb7f9eb90500e62ec9f48c6c3d63e
Molly Wilson <molly_wilson1@outlook.com>
2023-03-12 20:22:24 -0500

    cdk init and dynamo db init

---
+++ cdk/lib/day-planner-stack.ts
@@ -0,0 +1,15 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+
+export class DayPlannerStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+    new dynamodb.Table(this, "SimpleDynamoDB", {
+      partitionKey: {
+        name: "id",
+        type: dynamodb.AttributeType.STRING
+      },
+    })
+  }
+}
