570ca8bc6d51741b9edc6ab31a5f7adec83797cc
Molly Wilson <molly_wilson1@outlook.com>
2023-03-26 16:57:36 -0500

    debug 2

--- cdk/bin/day-planner.ts
+++ cdk/bin/day-planner.ts
@@ -7,7 +7,7 @@ const { promisify } = require('util');
 const exec = promisify(require('child_process').exec)
 
 async function createStack() {
-  console.log(process.env.CODEBUILD_SOURCE_VERSION)
+  console.log(process.env.CODEBUILD_WEBHOOK_HEAD_REF)
   const branchCommand = await exec('git branch --show-current'); 
   const branchName = branchCommand.stdout.trim(); 
   console.log(branchName);
