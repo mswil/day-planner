156f56d9053ff17c71a748e1040f1d6fbb38cb3b
Molly Wilson <molly_wilson1@outlook.com>
2023-04-06 15:03:56 -0500

    code cleanup

--- cdk/bin/day-planner.ts
+++ cdk/bin/day-planner.ts
@@ -7,15 +7,8 @@ const { promisify } = require('util');
 const exec = promisify(require('child_process').exec)
 
 async function createStack() {
-  //   "CODEBUILD_INITIATOR": "codepipeline/DayPlannerCDKPipeline-cdk-branching-strategy",
-  console.log("LOOK HERE " + process.env.CODEBUILD_INITIATOR)
-  // const branchCommand = await exec('git branch --show-current'); 
-  // const branchName = branchCommand.stdout.trim(); 
-
   const codebuildInitiator = process.env.CODEBUILD_INITIATOR
   const branchName = codebuildInitiator ? codebuildInitiator.substring(codebuildInitiator.indexOf("-") + 1) : "main";
-  // check if env exists
-  console.log("branch name " + branchName);
   const app = new cdk.App();
   new DayPlannerStack(app, `DayPlannerStack-${branchName}`, branchName);
 }
