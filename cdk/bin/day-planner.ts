4dfa1138df54b6db7848c3beea18bde5a8cd9421
Molly Wilson <molly_wilson1@outlook.com>
2023-04-06 14:01:20 -0500

    debug 5

--- cdk/bin/day-planner.ts
+++ cdk/bin/day-planner.ts
@@ -9,8 +9,10 @@ const exec = promisify(require('child_process').exec)
 async function createStack() {
   //   "CODEBUILD_INITIATOR": "codepipeline/DayPlannerCDKPipeline-cdk-branching-strategy",
   console.log("LOOK HERE " + JSON.stringify(process.env.CODEBUILD_INITIATOR))
-  const branchCommand = await exec('git branch --show-current'); 
-  const branchName = branchCommand.stdout.trim(); 
+  // const branchCommand = await exec('git branch --show-current'); 
+  // const branchName = branchCommand.stdout.trim(); 
+
+  const branchName = JSON.stringify(process.env.CODEBUILD_INITIATOR).split("/",2)[1];
   console.log("branch name " + branchName);
   const app = new cdk.App();
   new DayPlannerStack(app, `DayPlannerStack-${branchName}`, branchName);
