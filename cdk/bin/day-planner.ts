ae6a196fb2d676602996262f68c3f34e2ce30880
Molly Wilson <molly_wilson1@outlook.com>
2023-04-06 21:18:42 -0500

    Move createStack() logic out of function definition as async is no longer necessary. Document what code does and what actions the developer must take. Change buildImage so that npm doesn't have to be installed every commit

--- cdk/bin/day-planner.ts
+++ cdk/bin/day-planner.ts
@@ -3,14 +3,9 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { DayPlannerStack } from '../lib/day-planner-stack';
 
-const { promisify } = require('util');
-const exec = promisify(require('child_process').exec)
-
-async function createStack() {
-  const codebuildInitiator = process.env.CODEBUILD_INITIATOR
-  const branchName = codebuildInitiator ? codebuildInitiator.substring(codebuildInitiator.indexOf("-") + 1) : "main";
-  const app = new cdk.App();
-  new DayPlannerStack(app, `DayPlannerStack-${branchName}`, branchName);
-}
-
-createStack();
+// Creates a new Stack named after the current branch being modified during the codebuild process
+// Will need to run cdk deploy after pushing a new branch
+const codebuildInitiator = process.env.CODEBUILD_INITIATOR
+const branchName = codebuildInitiator ? codebuildInitiator.substring(codebuildInitiator.indexOf("-") + 1) : "main";
+const app = new cdk.App();
+new DayPlannerStack(app, `DayPlannerStack-${branchName}`, branchName);
--- cdk/lib/day-planner-stack.ts
+++ cdk/lib/day-planner-stack.ts
@@ -2,8 +2,8 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
-import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 import * as sm from "aws-cdk-lib/aws-secretsmanager";
+import { LinuxBuildImage } from 'aws-cdk-lib/aws-codebuild';
 
 export class DayPlannerStack extends Stack {
   constructor(scope: Construct, id: string, branch: string = 'main', props?: StackProps) {
@@ -15,15 +15,18 @@ export class DayPlannerStack extends Stack {
         input: CodePipelineSource.gitHub('mswil/day-planner', branch, { // replace the GitHub repository name with 'user-name/repository-name'
           authentication: sm.Secret.fromSecretNameV2(this, "github-token", "github-token-other").secretValue
         }), 
-        
         // The build steps for the pipeline are defined by these commands
-        installCommands: ['npm i -g npm@latest'],
         commands: ['cd cdk',
                     'npm ci',
                     'npm run build',
                     'npx cdk synth'],
         primaryOutputDirectory: "cdk/cdk.out"
       }),
+      codeBuildDefaults: {
+        buildEnvironment: {
+          buildImage: LinuxBuildImage.AMAZON_LINUX_2_4
+        }
+      }
     })
 
     // new dynamodb.Table(this, "SimpleDynamoDB", {
