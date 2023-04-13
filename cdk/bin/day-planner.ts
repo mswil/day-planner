a6940e498897da3e3215f852f7dbd056a595d2fc
Molly Wilson <molly_wilson1@outlook.com>
2023-04-12 20:41:06 -0500

    supporting stack creation off of new github branches

--- cdk/bin/day-planner.ts
+++ cdk/bin/day-planner.ts
@@ -3,9 +3,25 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { DayPlannerStack } from '../lib/day-planner-stack';
 
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec)
+
 // Creates a new Stack named after the current branch being modified during the codebuild process
 // Will need to run cdk deploy after pushing a new branch
-const codebuildInitiator = process.env.CODEBUILD_INITIATOR
-const branchName = codebuildInitiator ? codebuildInitiator.substring(codebuildInitiator.indexOf("-") + 1) : "main";
-const app = new cdk.App();
-new DayPlannerStack(app, `DayPlannerStack-${branchName}`, branchName);
+async function computeBranchName() {
+  const codebuildInitiator = process.env.CODEBUILD_INITIATOR
+
+  let branchName = 'main';
+  if (!codebuildInitiator) {
+    const localBranchName = await exec("git branch --show-current");
+    branchName = localBranchName.stdout.trim()
+  } else {
+    branchName = codebuildInitiator ? codebuildInitiator.substring(codebuildInitiator.indexOf("-") + 1) : "main";
+  }
+  return branchName
+}
+
+computeBranchName().then(branchName => {
+  const app = new cdk.App();
+  new DayPlannerStack(app, `DayPlannerStack-${branchName}`, branchName);
+})
