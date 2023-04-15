334fbd312c30a3af6111b890aeadf09d37739f8e
Molly Wilson <molly_wilson1@outlook.com>
2023-04-15 11:20:05 -0500

    add securityGroup, ec2 instance, and bash script for installing apache

--- cdk/lib/day-planner-ec2-stack.ts
+++ cdk/lib/day-planner-ec2-stack.ts
@@ -1,12 +1,33 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Function, InlineCode, Runtime } from 'aws-cdk-lib/aws-lambda';
-import { Vpc } from 'aws-cdk-lib/aws-ec2';
+import { AmazonLinuxGeneration, AmazonLinuxImage, Instance, InstanceClass, InstanceSize, InstanceType, Peer, Port, SecurityGroup, Vpc } from 'aws-cdk-lib/aws-ec2';
+import {readFileSync} from 'fs';
 
 export class Ec2Stack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
       super(scope, id, props);
 
-      new Vpc(this, 'Vpc', {});
+      const vpc = new Vpc(this, 'Vpc', {});
+
+      const securityGroup = new SecurityGroup(this, "SecurityGroup", {
+        vpc
+      })
+
+      securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(22), "SSH");
+      securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(80), "HTTP");
+
+      const ec2Instance = new Instance(this, "Ec2Instance", {
+        instanceType: InstanceType.of(InstanceClass.T2, InstanceSize.MICRO),
+        machineImage: new AmazonLinuxImage({
+          generation: AmazonLinuxGeneration.AMAZON_LINUX_2
+        }),
+        vpc,
+        securityGroup
+      })
+
+      const userDataScript = readFileSync("./lib/user-data.sh" , 'utf8')
+
+      ec2Instance.addUserData(userDataScript)
+
     }
 }
